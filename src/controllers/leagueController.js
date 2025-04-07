684c4571ebf57702d2507560ef2defba679159dc
Dominik Chovanec <dominkchov@gmail.com>
2025-04-07 21:54:00 +0200

    Add legue GET

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -1,7 +1,17 @@
-const { selectLeague, insertLeague, updateLeague, deleteLeague } = require("../models/leagueModel")
+const leagueModel = require("../models/leagueModel");
+
 
 const getLeague = async (req, res) => {
+    try {
+        const result = await leagueModel.selectLeague();
+        if (result.rows.length < 1)
+            return res.status(200).send({ message: "V databáze sa nenachádzajú žiadne ligy" });
 
+        return res.status(200).json(result.rows);
+    } catch (e) {
+        console.log(`🟠 We got a problem: ${e}`);
+        return res.status(500).send({message: "Neocakavana chyba na strane databazy."});
+    }
 };
 const createLeague = async (req, res) => {
 
