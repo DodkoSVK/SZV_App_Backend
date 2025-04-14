31d63ed328bc403ee8c975ade92b978bd698eb18
Dominik Chovanec <dominkchov@gmail.com>
2025-04-14 19:40:52 +0200

    Change security

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/* const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false, // ak nechceš riešiť certifikáty
+    },
+}); */
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('connect', () => {
