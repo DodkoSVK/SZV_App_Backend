e50b07e50bab9f266919776d50cc2dd9d3588e59
Dominik Chovanec <dominkchov@gmail.com>
2025-04-16 07:43:06 +0200

    Edit for ssh tunnel

--- config/database.js
+++ config/database.js
@@ -1,19 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-/* const pool = new Pool({
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false, // ak nechceš riešiť certifikáty
+        rejectUnauthorized: true, 
     },
-}); */
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+}); 
 
 pool.on('connect', () => {
     console.log('Connected to DB');
